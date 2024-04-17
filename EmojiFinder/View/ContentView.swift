a9e910673065bbdbbd6c42f6ae5b096e8aa1ea81
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-17 11:11:21 +0700

    Second

---
+++ EmojiFinder/View/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  EmojiFinder
+//
+//  Created by MacBook Pro on 17/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis: [Emoji] = EmojiProvider.allEmojis()
+    
+    @State private var searchText: String = ""
+    
+    var emojiSearchResults: [Emoji] {
+        guard !searchText.isEmpty else {
+            return emojis
+        }
+        return emojis.filter { emoji in
+            emoji.name.lowercased()
+                .contains(searchText.lowercased())
+        }
+    }
+    
+    
+    
+    
+    var body: some View {
+        NavigationStack {
+            List(emojiSearchResults) { emoji in
+                EmojiRow(emoji: emoji)
+//             .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Emoji")
+//            .listStyle(.plain)
+            .searchable(text: $searchText, placement:  .navigationBarDrawer(displayMode: .always),
+                        prompt: "What emoji's that you're looking for?"
+            )
+            .overlay {
+                if emojiSearchResults.isEmpty {
+                    ContentUnavailableView.search(text: searchText)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
